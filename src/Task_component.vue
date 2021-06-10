4755fa7dd79865ee5ef86ea29be7ca542c01933f
pooja v <pooja.v@mobiotics.com>
2021-06-10 17:48:28 +0530

    Changes made to the dynamic array push & to show odd & even no.

--- src/Task_component.vue
+++ src/Task_component.vue
@@ -6,23 +6,48 @@
   <green-component></green-component>
   <yellow-component></yellow-component>
   <blue-component></blue-component>
+
+<br><br>
+<label for="array">Please enter array value</label>
+  <input type="text" name="array" id="array" v-model="arrayData" />
+
+  <div v-for="item in final_array" :key="item">
+    <div v-if="item % 2 == 0">
+      <h3>{{ item }} is an {{ even }} number</h3>
+    </div>
+    <div v-if="item % 2 != 0">
+      <h3>{{ item }} is an {{ odd }} number</h3>
+    </div>
+  </div>
 </template>
 <script>
-import RedComponent from './components/RedComponent.vue';
-import GreenComponent from './components/GreenComponent.vue';
-import YellowComponent from './components/YellowComponent.vue';
-import BlueComponent from './components/BlueComponent.vue'
+import RedComponent from "./components/RedComponent.vue";
+import GreenComponent from "./components/GreenComponent.vue";
+import YellowComponent from "./components/YellowComponent.vue";
+import BlueComponent from "./components/BlueComponent.vue";
 
 export default {
   components: {
-   'red-component': RedComponent,
-   'green-component': GreenComponent,
-   'yellow-component': YellowComponent,
-   'blue-component': BlueComponent
+    "red-component": RedComponent,
+    "green-component": GreenComponent,
+    "yellow-component": YellowComponent,
+    "blue-component": BlueComponent,
   },
   data() {
-    return {};
+    return {
+      arrayData: "",
+      even: "EVEN",
+      odd: "ODD",
+    };
   },
-  methods: {},
+  computed: {
+    final_array() {
+      if (this.arrayData.length) {
+        return this.arrayData.split(",");
+      } else {
+        return null;
+      }
+    },
+  },
 };
 </script>
